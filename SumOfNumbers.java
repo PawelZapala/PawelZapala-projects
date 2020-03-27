2feb3b60d509a6805bc33f19e19ba821f0b0d71a
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-03-27 18:43:31 +0100

    Add files via upload

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -1,9 +1,9 @@
-public class AverageValue {
+public class SumOfNumbers {
     public static void main(String[] args) {
         int sum = 0;
         for (String arg : args) {
             sum += Integer.parseInt(arg);
         }
-        System.out.println(sum/args.length);
+        System.out.println(sum);
     }
 }
