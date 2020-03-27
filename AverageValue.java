b25821e2a79fd1a4a7d237d66728ca75c19bf4e2
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-03-27 18:50:33 +0100

    Add files via upload

--- AverageValue.java
+++ AverageValue.java
@@ -1,8 +1,8 @@
 public class AverageValue {
     public static void main(String[] args) {
-        int sum = 0;
+        double sum = 0;
         for (String arg : args) {
-            sum += Integer.parseInt(arg);
+            sum += Double.parseDouble(arg);
         }
         System.out.println(sum/args.length);
     }
