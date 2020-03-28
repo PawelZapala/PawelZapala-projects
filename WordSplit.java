e083cad105be335ae09e036932b5e8ee6919d570
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-03-28 12:22:40 +0100

    Add files via upload

--- WordSplit.java
+++ WordSplit.java
@@ -1,6 +1,6 @@
 public class WordSplit {
         public static void main(String[] args){
-                    String s = "Test";
+            String s = args[0];
                     char[] word = s.toCharArray();
                     for (int i = 0; i < word.length; i++) {
                         System.out.println(word[i]);
