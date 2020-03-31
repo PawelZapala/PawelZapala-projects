991217fc25997fc2e16c4f290f119b43e9c916e8
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-03-31 22:36:06 +0200

    Add files via upload

--- Cat.java
+++ Cat.java
@@ -5,18 +5,19 @@ public class Cat {
         this.name = name;
     }
 
-    public void isHungry() {
-        System.out.println(name + " is hungry");
-    }
-
     public void play(String toyName) {
-        System.out.println(name + " is playing around with " + toyName);
+    System.out.println(name + " is playing around with " + toyName);
     }
 
     public void eat(String mealName) {
         System.out.println(name + " is eating " + mealName);
+        boolean isHungry = false;
+        System.out.println(name + " is not hungry");
     }
+
     public void sleep(Integer sleepTime) {
         System.out.println(name + " is going to sleep for " + sleepTime + " hours");
+        boolean isHungry = true;
+        System.out.println(name + " is hungry");
     }
 }
