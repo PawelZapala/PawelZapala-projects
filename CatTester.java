3b71438ffb4e843303e2ab19e0973827a9ad1f9f
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-03-31 22:12:32 +0200

    Add files via upload

---
+++ CatTester.java
@@ -0,0 +1,13 @@
+public class CatTester {
+    public static void main(String[] args) {
+            Cat tom = new Cat("Tom");
+
+        tom.eat("Milk");
+        tom.eat("Mouse");
+        tom.isHungry();
+        tom.sleep(5);
+        tom.eat("Mouse");
+        tom.play("Ball");
+
+        }
+}
