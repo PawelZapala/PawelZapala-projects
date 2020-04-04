f282f68474ad00c8831b9e37acbec13d5231b12d
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-04-04 13:00:11 +0200

    Add files via upload

--- MobilePhone.java
+++ MobilePhone.java
@@ -1,17 +1,18 @@
 public class MobilePhone {
     private String brand;
     private String model;
-    private int batteryCapacity;
+    private final int batteryCapacity;
     private int batteryAmount;
-    private int memory;
+    private final int memory;
     private int freeMemory;
-
 
     public MobilePhone(String brand, String model, int bc, int m) {
         this.brand = brand;
         this.model = model;
-        batteryCapacity = bc;
-        memory = m;
+        this.batteryCapacity = bc;
+        this.batteryAmount = bc;
+        this.memory = m;
+        this.freeMemory = m;
     }
 
     public void chargePhone() {
@@ -19,24 +20,26 @@ public class MobilePhone {
         System.out.println(brand + model + " has been charged. Remain battery capacity: " + batteryCapacity + "mAh");
         }
 
-    public void use(String applicationName, int n){
-        batteryAmount = batteryCapacity - n*100;
-        if (batteryAmount >= 0) {
-            batteryCapacity = batteryAmount - n*100;
-            System.out.println(applicationName + " has been used for 5 hours on " + n + " hours on " + brand + model + ". Remain battery capacity: " + batteryAmount + "mAh");
-        } else {
-            System.out.println(applicationName + " can't be run on " + brand + model + ". Phoned discharged");
+    public void use(String applicationName, int hoursQuantity){
+        var batteryAmountRequired = hoursQuantity * 100;
+
+        if (batteryAmount >= batteryAmountRequired) {
+            batteryAmount -= batteryAmountRequired;
+            System.out.println(applicationName + " has been used for " + hoursQuantity +" hours on " + brand + model + ". Remain battery capacity: " + batteryAmount + " mAh");
+        } else if (batteryAmount == 0){
+            System.out.println(applicationName + " can't be run on " + brand + model + ". Phone discharged" );
+             } else {
+            System.out.println(applicationName + " has been used for " + batteryAmount / 100 + " hours on " + brand + model + ". Phone has been discharged.");
+            batteryAmount = 0;
         }
     }
 
     public void install(String applicationName, int appMemory) {
-        freeMemory = memory - appMemory;
-        if (freeMemory < 0){
-            memory = freeMemory + appMemory;
-            System.out.println(applicationName + " can't be installed on " + brand + model + ". There are no memory.");
+        if (freeMemory >= appMemory) {
+            freeMemory -= appMemory;
+            System.out.println(applicationName + " has been installed on " + brand + model + ". Remain memory " + freeMemory + "MB");
         } else {
-            memory = freeMemory - appMemory;
-            System.out.println(applicationName + " has been installed on " + brand + model);
+            System.out.println(applicationName + " can't be installed on " + brand + model + ". There are no memory.");
         }
     }
 }
