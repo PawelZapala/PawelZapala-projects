1c574b10a2529758593aefd8714a13081b397dd0
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-04-07 22:54:23 +0200

    Add files via upload

--- Engine.java
+++ Engine.java
@@ -1,38 +1,49 @@
 public class Engine {
     private String name;
-    private int horsePower;
-    private int capacity;
 
     public String getName() {
         return name;
     }
 
+    private int horsePower;
+    private int capacity;
+    private boolean started;
+
+    public boolean isStarted() {
+        return started;
+    }
 
     public int getCapacity() {
         return capacity;
-    }
-
-    private boolean isStarted = false;
-
-    public boolean isStarted() {
-        return isStarted;
-    }
-
-    public void setStarted(boolean started) {
-        isStarted = started;
     }
 
     public int getHorsePower() {
         return horsePower;
     }
 
-    public void setHorsePower(int horsePower) {
-        this.horsePower = horsePower;
-    }
-
     public Engine(String name, int hp, int capacity) {
         this.name = name;
         this.horsePower = hp;
         this.capacity = capacity;
+    }
+
+    public void startEngine() {
+        if (!started) {
+            System.out.println(" is starting " + name + " engine.");
+            System.out.println(name + " engine has been started.");
+            started = true;
+        } else {
+            System.out.println(name + " engine has been already started in ");
+        }
+    }
+
+    public void stopEngine() {
+        if (started) {
+            System.out.println(" is stopping " + name + " engine.");
+            System.out.println(name + " has been stopped.");
+            started = false;
+        } else {
+            System.out.println(name + " has been already stopped.");
+        }
     }
 }
