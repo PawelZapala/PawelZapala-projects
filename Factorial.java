b3373d3c05b1e34e9749e9e19ec6e1af1d14881b
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-04-09 18:21:09 +0200

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+package AlgoritmTasks;
+public class Factorial {
+    public static long factorial(long n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.print(factorial(i) + " ");
+        }
+    }
+}
