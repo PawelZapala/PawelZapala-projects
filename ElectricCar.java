12caa1f422b65ac0b71b576f32187937c3e2a3af
PawelZapala <62570108+PawelZapala@users.noreply.github.com>
2020-04-14 20:35:50 +0200

    Add files via upload

--- ElectricCar.java
+++ ElectricCar.java
@@ -1,11 +1,11 @@
 package Interfaces;
 
-public class ElectricCar extends Car2 implements Chargeable {
+public class ElectricCar extends Car implements Chargeable {
     public ElectricCar (String manufacture, String model){
         super(manufacture, model);
     }
     @Override
     public void charge(){
-        System.out.println("Car is charging");
+        System.out.println(getManufacture() + " " + getModel() + "  is charging");
     }
 }
