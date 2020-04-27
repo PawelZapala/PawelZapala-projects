8c87a6426f153c272c5a65b9619c7e68925e2ee8
PawelZapala <pawelzapala007@gmail.com>
2020-04-27 22:52:41 +0200

    initial commit

---
+++ src/AlgoritmTasks/LetterCounter.java
@@ -0,0 +1,28 @@
+package AlgoritmTasks;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LetterCounter {
+    public static void counter (String str) {
+        str = str.replace("", "");
+        Map counter = new LinkedHashMap<Character, Integer>();
+        for (char ch : str.toCharArray()){
+            if (counter.containsKey(ch)){
+                counter.put(ch, (Integer) counter.get(ch) + 1);
+            } else {
+                counter.put(ch, 1);
+            }
+        }
+        System.out.println(counter);
+    }
+
+    public static void main(String[] args) {
+        String text = "This is a test text";
+        counter(text);
+    }
+}
+
+
+
+
