5652aff5c7532bd64c60a8c92b0eda3560f5edc9
PawelZapala <pawelzapala007@gmail.com>
2020-04-27 22:07:43 +0200

    initial commit

--- src/CollectionFramework/UniqueLetters.java
+++ src/CollectionFramework/UniqueLetters.java
@@ -1,23 +1,18 @@
 package CollectionFramework;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+
 
 public class UniqueLetters {
     public static void main(String[] args) {
-        Set<String> letters = new HashSet<>();
-        letters.add("T");
-        letters.add("h");
-        letters.add("i");
-        letters.add("s");
-        letters.add("i");
-        letters.add("s");
-        letters.add("a");
-        letters.add("t");
-        letters.add("e");
-        letters.add("x");
-        letters.add("t");
 
-        System.out.println(letters);
+        String text = "This is a test text";
+        String[] chars = text.split("");
+        Set<String> uniqueChars = new LinkedHashSet<>();
+        for (String s : chars) {
+            uniqueChars.add(s);
+        }
+        System.out.println(uniqueChars);
     }
 }
