5652f441452ca25c9eba026bc6e31ff5766d9b60
PawelZapala <pawelzapala007@gmail.com>
2020-05-26 21:36:57 +0200

    initial commit

--- src/Generics/BoxesandShelves/Item.java
+++ src/Generics/BoxesandShelves/Item.java
@@ -1,9 +1,40 @@
 package Generics.BoxesandShelves;
 
+import java.util.Objects;
+
 public abstract class Item {
     private String name;
 
     public Item(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+        Item item = (Item) o;
+        return Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
 }
