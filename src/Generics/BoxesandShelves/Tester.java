8b079a1755b0da723ffeca33f9f98978d515b5d1
PawelZapala <pawelzapala007@gmail.com>
2020-05-27 17:10:31 +0200

    initial commit

--- src/Generics/BoxesandShelves/Tester.java
+++ src/Generics/BoxesandShelves/Tester.java
@@ -4,10 +4,17 @@ package Generics.BoxesandShelves;
 public class Tester {
     public static void main(String[] args) {
         Box<Toy> toysBox = new Box<>();
-        toysBox.add("toy");
+        toysBox.add(new Toy("LEGO"));
+        toysBox.add(new Toy("Hot wheels"));
         Box<Food> foodsBox = new Box<>();
+        foodsBox.add(new Food("Bułka"));
+        foodsBox.add(new Food("Banan"));
         Box<Book> booksBox = new Box<>();
+        booksBox.add(new Book("JAVA. HEAD FIRST"));
+        booksBox.add(new Book("Czysty kod"));
         Box<Fantasy> fantasyBooksBox = new Box<>();
+        fantasyBooksBox.add(new Fantasy("Harry Potter"));
+        fantasyBooksBox.add(new Fantasy("The Lord of the rings"));
 
         Shelf<Toy> shelf1 = new Shelf<>();
         shelf1.add(toysBox);
